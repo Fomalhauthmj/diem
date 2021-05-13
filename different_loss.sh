6bebfe3b289f88f64954e6b9b6ded18861894d3c
Hemengjie <njtech_hemengjie@qq.com>
2021-05-13 11:20:19 +0800

    update scripts

--- different_loss.sh
+++ different_loss.sh
@@ -1,9 +1,11 @@
-num=$(awk 'BEGIN{for(i=0;i<=1;i+=0.01)print i}')
-rm /home/hmj/temp/report.txt
+num=$(awk 'BEGIN{for(i=0;i<=0.7;i+=0.05)print i}')
+rm /home/hmj/temp/report
+echo "Loss,Method,mean(submitted),mean(committed),mean(expired),mean(latency),mean(p99latency),median(submitted),median(committed),median(expired),median(latency),median(p99latency),pstdev(submitted),pstdev(committed),pstdev(expired),pstdev(latency),pstdev(p99latency)" > /home/hmj/temp/report
+chmod a+x /home/hmj/temp/report
 for i in $num
 do 
-     echo "Loss:$i"
-     sudo /home/hmj/diem/repeat_swarm_test.sh $i
+     echo "current Loss:$i"
+     sudo /home/hmj/diem/repeat_swarm_test_random.sh $i
 done
 # 生成原始数据
 # cat temp/report.txt | rg "Average rate committed: " | sed 's/Average rate committed: \(.*\) txn\/s/\1/g'> temp/execl_data
--- repeat_swarm_test_random.sh
+++ repeat_swarm_test_random.sh
@@ -1,19 +1,26 @@
 #! /bin/bash
+
 # tmux会话
-session1="diem-swarm-LeaderReputation"
-session2="diem-swarm-RotatingProposer"
+session1="diem-swarm-LR"
+session2="diem-swarm-RP"
+
 # 终止可能存在的测试会话
 tmux kill-session -t $session1
 tmux kill-session -t $session2
+
 # 读入设定Loss
 Loss=$1
 echo "当前Loss：$Loss"
+
 # 删除测试结果文件
-rm /home/hmj/temp/LeaderReputation.txt /home/hmj/temp/RotatingProposer.txt
+rm /home/hmj/temp/temp_test_result_LR
+rm /home/hmj/temp/temp_test_result_RP
+
 cd /home/hmj/diem
-for i in {1..5}; 
+
+for i in {1..10}; 
 do 
-    echo "Test $i LeaderReputation part:"
+    echo "Test $i LR part:"
     echo "尝试启动本地集群，启动信息写入临时文件：/home/hmj/temp/temp_swarm_info"
     tmux new-session -d -s $session1 -n swarm 'target/release/diem-swarm-lr --diem-node target/release/diem-node -n 4 > /home/hmj/temp/temp_swarm_info'
     sleep 20s
@@ -43,7 +50,7 @@ do
     # 当前干扰规则
     iptables -L INPUT -n
     # 启动集群测试
-    echo "$cluster_test" | xargs target/release/cluster-test >> /home/hmj/temp/LeaderReputation.txt
+    echo "$cluster_test" | xargs target/release/cluster-test >> /home/hmj/temp/temp_test_result_LR
     # 删除干扰规则
     echo "$effect_port" | xargs iptables -D INPUT -p tcp -m statistic --mode random --probability $Loss -j DROP --dport
     # 终止测试会话
@@ -51,7 +58,7 @@ do
     # 删除测试目录
     rm -rf "$swarm_path"
 
-    echo "Test $i RotatingProposer part:"
+    echo "Test $i RP part:"
     echo "尝试启动本地集群，启动信息写入临时文件：/home/hmj/temp/temp_swarm_info"
     tmux new-session -d -s $session2 -n swarm 'target/release/diem-swarm-rp --diem-node target/release/diem-node -n 4 > /home/hmj/temp/temp_swarm_info'
     sleep 20s
@@ -81,7 +88,7 @@ do
     # 当前干扰规则
     iptables -L INPUT -n
     # 启动集群测试
-    echo "$cluster_test" | xargs target/release/cluster-test >> /home/hmj/temp/RotatingProposer.txt
+    echo "$cluster_test" | xargs target/release/cluster-test >> /home/hmj/temp/temp_test_result_RP
     # 删除干扰规则
     echo "$effect_port" | xargs iptables -D INPUT -p tcp -m statistic --mode random --probability $Loss -j DROP --dport
     # 终止测试会话 
@@ -90,10 +97,19 @@ do
     rm -rf "$swarm_path"
 done
 # 设置修改权限
-chmod a+w /home/hmj/temp/LeaderReputation.txt
-chmod a+w /home/hmj/temp/RotatingProposer.txt
+chmod a+w /home/hmj/temp/temp_test_result_LR
+chmod a+w /home/hmj/temp/temp_test_result_RP
+
 # 汇总计算
-echo "Loss:$Loss LeaderReputation:">>/home/hmj/temp/report.txt
-cat /home/hmj/temp/LeaderReputation.txt | grep -P "Total.*" -o  | sed 's/.*, committed: \([0-9]*\), expired: 0/\1/g' | awk '{sum+=$1} END {print  "Average rate committed: " sum/NR/60 " txn/s"}'>>/home/hmj/temp/report.txt
-echo "Loss:$Loss RotatingProposer:">>/home/hmj/temp/report.txt
-cat /home/hmj/temp/RotatingProposer.txt | grep -P "Total.*" -o  | sed 's/.*, committed: \([0-9]*\), expired: 0/\1/g' | awk '{sum+=$1} END {print  "Average rate committed: " sum/NR/60 " txn/s"}'>>/home/hmj/temp/report.txt
+rm /home/hmj/temp/temp_data_file_LR
+echo "submitted committed expired latency p99latency" > /home/hmj/temp/temp_data_file_LR
+cat /home/hmj/temp/temp_test_result_LR | grep -P "Average.*" -o | sed 's/Average rate: submitted: \([0-9]*\) txn\/s, committed: \([0-9]*\) txn\/s, expired: \([0-9]*\) txn\/s, latency: \([0-9]*\) ms, p99 latency: \([0-9]*\) ms/\1 \2 \3 \4 \5/g' >> /home/hmj/temp/temp_data_file_LR
+output_lr=`cat /home/hmj/temp/temp_data_file_LR | datamash --whitespace --header-in --round 2 --output-delimiter=, mean 1,2,3,4,5 median 1,2,3,4,5 pstdev 1,2,3,4,5`
+echo "$Loss,LR,$output_lr" >> /home/hmj/temp/report
+rm /home/hmj/temp/temp_data_file_RP
+echo "submitted committed expired latency p99latency" > /home/hmj/temp/temp_data_file_RP
+cat /home/hmj/temp/temp_test_result_RP | grep -P "Average.*" -o | sed 's/Average rate: submitted: \([0-9]*\) txn\/s, committed: \([0-9]*\) txn\/s, expired: \([0-9]*\) txn\/s, latency: \([0-9]*\) ms, p99 latency: \([0-9]*\) ms/\1 \2 \3 \4 \5/g' >> /home/hmj/temp/temp_data_file_RP
+output_rp=`cat /home/hmj/temp/temp_data_file_RP | datamash --whitespace --header-in --round 2 --output-delimiter=, mean 1,2,3,4,5 median 1,2,3,4,5 pstdev 1,2,3,4,5`
+echo "$Loss,RP,$output_rp" >> /home/hmj/temp/report
+
+
